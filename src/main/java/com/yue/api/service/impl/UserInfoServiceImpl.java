e9039167c81b496d1f90536f766ce5343480efc3
liliang <1582880360@qq.com>
2020-10-30 16:59:08 +0800

    【*】Service 层。

--- src/main/java/com/yue/api/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/yue/api/service/impl/UserInfoServiceImpl.java
@@ -1,4 +1,4 @@
-package com.yue.api.service;
+package com.yue.api.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yue.api.entity.UserInfo;
 import com.yue.api.mapper.UserInfoMapper;
+import com.yue.api.service.UserInfoService;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Repository;
@@ -15,22 +16,29 @@ import java.util.List;
 
 
 
-@CacheConfig(cacheNames = "userInfo")
+
 @Service
-public class UserInfoService extends ServiceImpl<UserInfoMapper, UserInfo> {
+//public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> {
+public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> implements UserInfoService {
 
-
-    @CachePut(key = "userInfoService")
+    @Override
     public List<UserInfo> selectAllList() {
         UserInfoMapper baseMapper = getBaseMapper();
         return baseMapper.selectList(null);
     }
 
+    @Override
     // 查询所有
     public List<UserInfo> selectAllList(int page) {
         UserInfoMapper baseMapper = getBaseMapper();
         Page<UserInfo> userPage = new Page<>(page,2,false);
         IPage<UserInfo> iPage = baseMapper.selectPage(userPage, null);
         return iPage.getRecords();
+    }
+
+    @Override
+    public int add(UserInfo info) {
+        UserInfoMapper baseMapper = getBaseMapper();
+        return baseMapper.insert(info);
     }
 }
