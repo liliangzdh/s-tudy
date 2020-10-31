b28986636689086c989015f68ab79a66e6fe9d0d
liliang <1582880360@qq.com>
2020-10-31 17:34:12 +0800

    【*】集成 swagger 。

--- src/main/java/com/yue/api/controller/TestController.java
+++ src/main/java/com/yue/api/controller/TestController.java
@@ -5,13 +5,15 @@ import com.google.gson.Gson;
 import com.yue.api.entity.UserInfo;
 import com.yue.api.service.RedisService;
 import com.yue.api.service.UserInfoService;
-import com.yue.api.service.impl.UserInfoServiceImpl;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = "测试模块", description = "测试模拟", value = "测试模拟")
 @RequestMapping("test")
 @RestController
 public class TestController {
@@ -23,9 +25,9 @@ public class TestController {
     @Autowired
     private RedisService redisService;
 
-
-    @RequestMapping("/page")
-    public String page(int page) {
+    @ApiOperation(value = "获取列表", notes = "获取列表")
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public String page(@ApiParam(name="page",value="第几页",required=true)int page) {
         System.out.println(" page:   " + page);
         List<UserInfo> userInfos = userInfoService.selectAllList(page);
 
@@ -34,14 +36,19 @@ public class TestController {
     }
 
 
-    @RequestMapping("/all")
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
     public String all() {
         List<UserInfo> userInfos = userInfoService.selectAllList();
         return new Gson().toJson(userInfos);
     }
 
 
-    @RequestMapping("/test")
+    @ApiOperation(value = "修改测试", notes = "车市")
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", value = "用户名", defaultValue = "String"),
+            @ApiImplicitParam(name = "password", value = "密码", defaultValue = "String")
+    })
     public String test() {
         List<UserInfo> name = (List<UserInfo>) redisService.get("userInfo");
 
@@ -55,7 +62,7 @@ public class TestController {
     }
 
 
-    @RequestMapping("/add")
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
     public void add() {
         UserInfo userInfo = new UserInfo();
         userInfo.setId((long) 9);
--- src/test/java/com/yue/api/SampleTest.java
+++ src/test/java/com/yue/api/SampleTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -64,12 +65,38 @@ public class SampleTest {
 
 
 
-        List<UserInfo> employees = userInfoMapper.selectList(
-                new QueryWrapper<UserInfo>()
-                        .eq("gender",0)
-                        .like("last_name","老师")
-                        //.or()//和or new 区别不大
-                        .like("email","a")
-        );
+//        List<UserInfo> employees = userInfoMapper.selectList(
+//                new QueryWrapper<UserInfo>()
+//                        .eq("gender",0)
+//                        .like("last_name","老师")
+//                        //.or()//和or new 区别不大
+//                        .like("email","a")
+//        );
+
+
+        File file = new File("/Users/liliang/Downloads/2020-10-31.log");
+        StringBuilder result = new StringBuilder();
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));//构造一个BufferedReader类来读取文件
+
+            String s = null;
+
+            String last = "";
+            while((s = br.readLine())!=null){//使用readLine方法，一次读一行
+//                result.append( System.lineSeparator() + s);
+
+                if(s!= null && s.indexOf("15879106056") != -1){
+                    System.out.println(last);
+                    System.out.println(s);
+                }
+
+                last = s;
+            }
+            br.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        System.out.println(file.exists());
     }
 }
